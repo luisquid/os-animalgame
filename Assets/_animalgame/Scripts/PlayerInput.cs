5f1335747567acbf3fa873ed8704dd42b010ae0f
Squid <luisbazan417@gmail.com>
2020-06-12 10:52:55 -0500

    First Commit

---
+++ Assets/_animalgame/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+    }
+}
